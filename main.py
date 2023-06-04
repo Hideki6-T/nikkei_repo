9aab3fa479a7d49048b6e7d0f4e38e66b58820f5
Hideki6-T <tiankou1127@ozzio.jp>
2023-06-04 11:20:54 +0900

    multiplicationを追加

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 def main():
-    print(add(2, 3))
-    print(sub(2, 1))
+    a, b = 1, 2
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{b} - {a} = {sub(b, a)}")
+    print(f"{a} * {b} = {mul(a, b)}")
 
 if __name__ == '__main__':
     main()
